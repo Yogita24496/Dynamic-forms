285c7093456c3b8306a1272e993b3a2f9d3883c5
Yogita Patil <yogita.patil@hurix.com>
2025-05-15 17:49:14 +0530

    comment out the app.test.js code

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-
-
+import React from 'react';
 import DynamicForm from './Form';
+
 function App() {
   return (
     <div className="App">
-    <h1>Dynamic Form Creation App</h1>
-    <main>
-        <DynamicForm />
-      </main>    
-  </div>
+      <h1>Dynamic Form Creation App</h1>
+      <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+        Learn React
+      </a>
+      <DynamicForm />
+    </div>
   );
 }
 
