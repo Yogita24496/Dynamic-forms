02153c85b38de657666d3f3e71f8aa6945a54533
Yogita Patil <yogita.patil@hurix.com>
2025-05-15 19:40:05 +0530

    added docker file

--- src/App.test.js
+++ src/App.test.js
@@ -1,3 +1,6 @@
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
 test('renders main app heading', () => {
   render(<App />);
   const headingElement = screen.getByRole('heading', { name: /Dynamic Form Creation App/i });
