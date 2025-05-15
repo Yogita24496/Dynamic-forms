7b4d0ba2b847deb9a184e9a0b3601d76a24ca97f
Yogita24496 <yogita.patil@hurix.com>
2025-05-15 17:52:50 +0530

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,5 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders app title', () => {
+test('renders main app heading', () => {
   render(<App />);
-  const titleElement = screen.getByText(/Dynamic Form Creation App/i);
-  expect(titleElement).toBeInTheDocument();
+  const headingElement = screen.getByRole('heading', { name: /Dynamic Form Creation App/i });
+  expect(headingElement).toBeInTheDocument();
 });
